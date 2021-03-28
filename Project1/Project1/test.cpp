ee09472be7822ca15718ee9bcb2e33e69d1023e2
c++ primer <757050468@qq.com>
2021-03-28 22:17:04 +0800

    9_43

--- Project1/Project1/test.cpp
+++ Project1/Project1/test.cpp
@@ -1,30 +1,9 @@
 #include "test.h"
 
-void test(string& s, string& oldVal, string& newVal)
-{
-	string :: iterator it1 = s.begin();
-	int len = oldVal.size();
-	for (it1; it1 != s.end() - len; it1++)
-	{
-		string tempS(it1, it1 + len);
-		if (tempS == oldVal)
-		{
-			it1 = s.erase(it1, it1+len);
-			it1 = s.insert(it1, newVal.begin(),newVal.end());
-			it1 += len;
-		}
-	}
-}
+
 int main() 
 {
-	string s("tho is a test vocabulary, thru is another vocabulary");
-	string s1 = "tho";
-	string s2 = "though";
-	test(s, s1, s2);
-	s1 = "thru";
-	s2 = "through";
-	test(s, s1, s2);
-	cout << s << endl;
+	
 
 	system("pause");
 	return 0;
--- Project1/Project1/test.h
+++ Project1/Project1/test.h
@@ -89,3 +89,30 @@ void test3_25() {
 		cout << c << " ";
 	}
 }
+
+//9.43
+void test9_43(string& s, string& oldVal, string& newVal)
+{
+	string::iterator it1 = s.begin();
+	int len = oldVal.size();
+	for (it1; it1 != s.end() - len; it1++)
+	{
+		string tempS(it1, it1 + len);
+		if (tempS == oldVal)
+		{
+			it1 = s.erase(it1, it1 + len);
+			it1 = s.insert(it1, newVal.begin(), newVal.end());
+			it1 += len;
+		}
+	}
+}
+
+//test case
+//string s("tho is a test vocabulary, thru is another vocabulary");
+//string s1 = "tho";
+//string s2 = "though";
+//test(s, s1, s2);
+//s1 = "thru";
+//s2 = "through";
+//test(s, s1, s2);
+//cout << s << endl;
